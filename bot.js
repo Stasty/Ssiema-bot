4406feef7f934e25d55d44c80026b77bc6e6fb3f
unknown <stasiu.ty@gmail.com>
2020-05-02 20:00:42 +0200

    komenda1

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ fs.readdir("./commands/", (err, files) => {
     }
 
     jsfile.forEach((f, i) => {
-        let props = require("./commands/${f}");
+        let props = require("./${f}");
         client.commands.set(props.help.name, props);
     });
 })
