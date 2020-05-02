1ecf3e823a6b9f158b777ff1c680dffa8520370c
unknown <stasiu.ty@gmail.com>
2020-05-02 15:56:31 +0200

    komenda1

--- bot.js
+++ bot.js
@@ -4,9 +4,24 @@ const Settings = require("./settings.json")
 const Words = ["Dobre, co nie?", "Marchew"];
 const komendy = ["Ssiema", "zmienprefix"];
 const token = process.env.Ssiema;
+const fs = require('fs');
 
+moodule.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply(Brak zgody);
+    if (!args[0]) return message.reply("No podaj go no");
 
+    let settings = JSON.parse(fs.readFileSync("./settings.json", "utf8"));
+    settings[message.guild.id] = {
+        prefix: args[0]
+    };
+    fs.writeFile("./settings.json", JSON.stringify(settings), (err) => { if (err) console.log(err); });
 
+    message.chanel.send("Prefix został zmieniony")
+}
+
+moodule.exports.help = {
+    name: "prefix"
+}
 
 client.on("ready", () => {
     console.log("I am ready!");
@@ -19,13 +34,15 @@ client.on("message", (message) => {
     else if (message.content == Words[1]) {
         message.reply("Mmmmmnom nom nom nom");
     }
-    else if (message.content.startsWith(Settings.prefix + komendy[1])) {
-        Settings.prefix = message.content - Settings.prefix - komendy[1] - ' ';
-        message.send(Settings.prefix + "to jest nowy prefix");
-    }
 });
 
-
+client.on("message", (message) => {
+    let settings = JSON.parse(fs.readFileSync("./settings.json", "utf8"));
+    settings[message.guild.id] = {
+        prefix: args[0]
+    };
+    let prefix = settigs[message.guild.id].prefix;
+});
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'ogólny');
     if (!channel) return;
